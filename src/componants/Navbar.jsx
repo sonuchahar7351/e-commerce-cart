3f5e61bd17159f63f9ec1b3e0d0b877aba6de306
sonuchahar7351 <sonuchahar7351@gmail.com>
2024-04-15 17:31:32 +0530

    some changes

--- src/componants/Navbar.jsx
+++ src/componants/Navbar.jsx
@@ -5,7 +5,7 @@ import { useSelector } from "react-redux";
 const Navbar = () => {
   const items=useSelector((state)=>state.cart)
   return (
-    <div className="flex items-center justify-between py-2 px-3 bg-red-300 h-14 w-full h-20">
+    <div className="flex items-center justify-between py-2 px-3 bg-red-300  w-full h-20">
       <span className="flex font-bold text-black-500">Redux Store</span>
      
         <div className="flex justify-between items-center">
